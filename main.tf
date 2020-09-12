3905ec718e261f4fcb0b5f0786fc912a7daba899
Sameer Meher <sam.tiku@gmail.com>
2020-09-12 17:09:49 +0530

    Remove S3 bucket

--- main.tf
+++ main.tf
@@ -4,13 +4,3 @@ provider "aws" {
   access_key = var.access_key
   secret_key = var.secret_key
 }
-
-resource "aws_s3_bucket" "b" {
-  bucket = "my-tf-bucket-sameer2020"
-  acl    = "private"
-
-  tags = {
-    Name        = "My bucket"
-    Environment = "Dev"
-  }
-}
